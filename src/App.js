3f517bec7a0a093e9736b1e092c6a7695bcee248
WillianAbrego <willianantonioabrego@gmail.com>
2022-10-02 15:23:21 -0600

    propiedades

--- src/App.js
+++ src/App.js
@@ -1,36 +1,45 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Componente from "./components/Componente";
+import Propiedades from "./components/Propiedades";
 function App() {
   return (
     <>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <section>
+            <img src={logo} className="App-logo" alt="logo" />
 
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
 
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </section>
+          <section>
+            <Componente msg="Hola soy un componente" />
+            <hr></hr>
+            <Propiedades
+              cadena="Esto una cadena de texto"
+              numero={20}
+              boolean={true}
+              arreglo={[1, 2, 3]}
+              objeto={{ nombre: "will", correo: "wil@mail" }}
+              funcion={(num) => num * num}
+              elementoReact={<i>Esto es un elemento React</i>}
+              componenteReact={
+                <Componente msg="soy un componente pasado como props" />
+              }
+            />
+          </section>
         </header>
-        <section>
-          <Componente msg="Hola soy un componente desde una prop funcional arrow" />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-        </section>
       </div>
       <div></div>
     </>
